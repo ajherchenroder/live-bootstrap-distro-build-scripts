cc98cf398a0747d9ae307e2afbb34d1566b02658
ajherchenroder <136374399+ajherchenroder@users.noreply.github.com>
2024-06-23 23:22:25 -0400

    fix typo in kernel build command

--- target/freebsdmk.sh
+++ target/freebsdmk.sh
@@ -37,7 +37,7 @@ cd /mnt/freebsd/usr/src/tools/build
 ./make.py -j $(nproc) TARGET=amd64 TARGET_ARCH=amd64 cleanworld
 ./make.py -j2 --host-bindir=/usr/bin TARGET=amd64 TARGET_ARCH=amd64 buildworld
 read -p 'press enter to continue ' PAUSE
-./make.py -j2 $(nproc) TARGET=amd64 TARGET_ARCH=amd64 buildkernel
+./make.py -j2 TARGET=amd64 TARGET_ARCH=amd64 buildkernel
 
 
 
