3e92655deb23019997a77a5f51d0a831f6afab33
ajherchenroder <136374399+ajherchenroder@users.noreply.github.com>
2024-06-28 13:24:14 -0400

    update License

--- target/gentoomk.sh
+++ target/gentoomk.sh
@@ -1,4 +1,21 @@
 #! /bin/bash
+# based on gentoo.txt located at https://mid-kid.root.sx/git/mid-kid/bootstrap in the gentoo-2024 folder
+# 
+#MIT License
+#
+#Copyright (c) 2017 Braintree
+#
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+#
+#The above copyright notice and this permission notice shall be included in all
+#copies or substantial portions of the Software.
+
+
 #set up working directory
 #
 #Notes:
