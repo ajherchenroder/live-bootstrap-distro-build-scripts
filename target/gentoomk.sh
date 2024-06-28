6bbbd71c87419a4543bb54646070e49239173137
ajherchenroder <136374399+ajherchenroder@users.noreply.github.com>
2024-06-27 20:15:05 -0400

    updated to configure portage and launch a bootstrap

--- target/gentoomk.sh
+++ target/gentoomk.sh
@@ -6,14 +6,14 @@
 #You need to be root to run this script
 #
 #decide where we are going to put things
-echo "This script can make a stag3 and snapshot tarball or setup and install Gentoo on another drive."
-read -p 'enter 1 for stage3/snapshot or 2 for full install (deafult is stage3/snapshot' FULLBUILD
+echo "This script can make a stage 3 and snapshot tarball or setup and install Gentoo on another drive."
+read -p 'enter 1 for stage 3/snapshot or 2 for full install (default is stage 3/snapshot > ' FULLBUILD
 mkdir /mnt/gentoo
 fdisk -l | grep /dev
 read -p 'Select the disk /dev node to build gentoo on. (sdxx) > ' DISKTOUSE
 mount /dev/$DISKTOUSE /mnt/gentoo
 if FULLBUILD==1 ; then
-   read -p 'Select the disk /dev node to install the final gentoo on. (sdxx) > ' DISKTOUSE2
+   read -p 'Select the disk /dev node to install the final gentoo on. (sdx) > ' DISKTOUSE2
 fi
 #download gentoo files
 mkdir /gentoosources
@@ -29,6 +29,42 @@ cd portage-3.0.65
 meson setup --prefix /usr build
 meson install -C build
 
+# Configure portage
+mkdir /mnt/tmp
+mkdir -p /var/db/repos/gentoo
+mount /gentoosources/gentoo-*.sqfs /mnt/tmp
+cp -avT /mnt/tmp /var/db/repos/gentoo
+umount /mnt/tmp
+rm -Rf /mnt/tmp
 
+# Set up passswd and group for portage
+echo 'portage:x:250:250:portage:/var/tmp/portage:/bin/false' >> /etc/passwd
+echo 'portage::250:portage' >> /etc/group
 
+#network and locale stuff
+echo 'nameserver 192.168.2.3' > /etc/resolv.conf
+#echo 'nameserver 1.1.1.1' > /etc/resolv.conf
+echo 'C.UTF-8 UTF-8' > /etc/locale.gen
 
+#make.conf
+cat > /etc/portage/make.conf << 'EOF'
+FEATURES='-news -pid-sandbox'
+CONFIG_PROTECT='-* /etc/locale.gen'
+USE='-nls'
+EOF
+
+##package.use.force
+#cat > /etc/portage/profile/package.use.force << 'EOF'
+#sys-devel/gcc -cxx
+#EOF
+
+#profile (set to ver 23)
+#mkdir -p /etc/portage/profile
+#ln -svr /var/db/repos/gentoo/profiles/default/linux/amd64/23.0 /etc/portage/make.profile
+
+# Install baselayout
+#emerge -O1 sys-apps/baselayout
+#source /etc/profile
+
+# Run bootstrap.sh
+/var/db/repos/gentoo/scripts/bootstrap.sh
