140290f122c7be372121076f57933015249a25f1
ajherchenroder <136374399+ajherchenroder@users.noreply.github.com>
2024-10-01 08:06:55 -0400

    Added buildroot script to rebuild the Live bootstrap UEFI environment

---
+++ target/buildroot.sh
@@ -0,0 +1,28 @@
+#!/usr//bin/bash
+#set -e
+mount -vt devtmpfs devtmpfs /dev
+mount -vt proc proc /proc
+mount -vt sysfs sysfs /sys
+mount -vt tmpfs tmpfs /run
+mount -t tmpfs -o nosuid,nodev tmpfs /dev/shm
+mount -vt devpts devpts -o mode=0625 /dev/pts
+chmod -r 777 /etc
+chmod -r 777 /sys
+chmod -r 777 /tmp
+chmod -r 777 /usr
+lsblk
+read -p "Enter the partition to build on (sdxx) -> " USEPART
+if ! test -d /mnt
+   mkdir /mnt
+fi
+if ! test -d /mnt/buildroot
+   mkdir /mnt/buildroot
+fi
+mount -v -t ext4 /dev/$USEPART /mnt/buildroot
+cd /mnt/buildroot
+git clone https://github.com/ajherchenroder/livebootstrap-buildroot.git
+cd livebootstrap-buildroot
+make olddefconfig
+make
+cp /mnt/buildroot/livebootstrap-buildroot/output/images/disk.img  /mnt/buildroot
+echo "the final file called disk.img is located in /mnt/buildroot"
