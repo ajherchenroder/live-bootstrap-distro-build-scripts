6ab621dad9a2b796c076266a376654dda6b22b4f
ajherchenroder <136374399+ajherchenroder@users.noreply.github.com>
2024-10-27 02:18:44 -0400

    missed a python piece

--- target/gentoomk.sh
+++ target/gentoomk.sh
@@ -120,7 +120,7 @@ mv /bin/bzip2 /bin/bzip2-reference
 ln -s bzip2-reference /bin/bzip2
 ./portage/bin/emerge -O1 app-arch/bzip2
 #./portage/bin/emerge -O1 =dev-lang/python-3.9.19_p3
-./portage/bin/emerge -O1 dev-lang/python-exec
+#./portage/bin/emerge -O1 dev-lang/python-exec
 
 # Install the rest of the dependencies for meson
 FETCHCOMMAND="$FETCHCOMMAND" ./portage/bin/emerge -O1 dev-python/gpep517
