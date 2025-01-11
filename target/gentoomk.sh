29350d041e1e05d41ee2e91cdc6b4c52691975a2
ajherchenroder <136374399+ajherchenroder@users.noreply.github.com>
2025-01-11 17:26:26 -0500

    fix references to old portage version

--- target/gentoomk.sh
+++ target/gentoomk.sh
@@ -86,11 +86,11 @@ rm -rf /var/db/repos/gentoo
 mv squashfs-root /var/db/repos/gentoo
 
 # Install temporary copy of portage
-tar xf /var/cache/distfiles/portage-3.0.65.tar.bz2
-cd portage-3.0.65
+tar xf /var/cache/distfiles/portage-3.0.66.1.tar.bz2
+cd portage-3.0.66.1
 patch -p1 -i ../portage.patch 
 cd ..
-ln -sf portage-3.0.65 portage 
+ln -sf portage-3.0.66.1 portage 
 
 # Add portage user/group
 echo 'portage:x:250:250:portage:/var/tmp/portage:/bin/false' >> /etc/passwd
