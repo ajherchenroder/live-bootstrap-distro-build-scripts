d701e4f6111eecd0b50cd8e82d5a4b55fcf39ab9
ajherchenroder <136374399+ajherchenroder@users.noreply.github.com>
2025-01-12 01:03:18 -0500

    typo fix

--- target/gentoomk.sh
+++ target/gentoomk.sh
@@ -134,7 +134,7 @@ FETCHCOMMAND="$FETCHCOMMAND" ./portage/bin/emerge -O1 net-misc/wget
 mv /bin/bzip2 /bin/bzip2-reference
 ln -s bzip2-reference /bin/bzip2
 ./portage/bin/emerge -O1 app-arch/bzip2
-FETCHCOMMAND="$FETCHCOMMAND" ./portage/bin/emerge -O1 =dev-lang/python:3.12
+FETCHCOMMAND="$FETCHCOMMAND" ./portage/bin/emerge -O1 dev-lang/python:3.12
 ./portage/bin/emerge -O1 dev-lang/python-exec
 
 # Install the rest of the dependencies for meson
