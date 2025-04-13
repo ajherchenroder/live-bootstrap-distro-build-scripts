e8ebff278d19791a27a8855049d6828caf3e7e61
ajherchenroder <136374399+ajherchenroder@users.noreply.github.com>
2025-04-13 15:49:32 -0400

    Functional improvements
    Updated the stage three build system to use a current snapshot
    Updated the stage three build system to better utilizes additional cores without crashing due to being out of memory. 
    The balance is currently set to 1 core per 4G of physical memory.

--- target/gentoomk3.sh
+++ target/gentoomk3.sh
@@ -14,8 +14,18 @@
 #
 #The above copyright notice and this permission notice shall be included in all
 #copies or substantial portions of the Software.
+MEM=$(free -h | awk '/Mem\:/ { print $2 }') 
+MEM="${MEM//G/}"
+MAXCOREM=$(echo "scale=0; $MEM / 4" | bc)
+if [ $(nproc) -ge $MAXCOREM ]; then
+   MAXCORE=$MAXCOREM
+else 
+   MAXCORE=$(nproc)
+fi
+
 emerge --sync
 emerge --oneshot sys-apps/portage
+emerge --ask --verbose --update --deep --changed-use @world
 emerge --depclean
 
 echo dev-util/catalyst >> /etc/portage/package.accept_keywords
@@ -28,8 +38,8 @@ ROOT="$PWD/stage" QUICKPKG_DEFAULT_OPTS=--include-config=y emerge --quickpkg-dir
 #break circular dependencies in stage 1
 ROOT="$PWD/stage" USE=-nls emerge sys-devel/m4
 ROOT="$PWD/stage" USE=-nls emerge sys-apps/help2man
-ROOT="$PWD/stage" MAKEOPTS=-j1 emerge =llvm-core/llvm-19.1.4
-ROOT="$PWD/stage" MAKEOPTS=-j1 emerge =llvm-core/llvm-18.1.8-r6
+ROOT="$PWD/stage" MAKEOPTS=-j$MAXCORE emerge =llvm-core/llvm-19.1.4
+ROOT="$PWD/stage" MAKEOPTS=-j$MAXCORE emerge =llvm-core/llvm-18.1.8-r6
 mkdir stage/etc/portage  # catalyst breaks otherwise...
 tar cf stage.tar -C stage .
 rm -rf stage
@@ -51,18 +61,21 @@ do
         L) REMOTE="local";; #download from the local repositories
      esac
 done
-if test "$REMOTE" = "local"; then 
-   echo "local"
-   wget http://192.168.2.102/gentoo/gentoo-20250101.xz.sqfs
-else
-   echo "remote"
-   wget http://distfiles.gentoo.org/snapshots/squashfs/gentoo-20250101.xz.sqfs
-fi
-mkdir -p /var/tmp/catalyst/snapshots
-unsquashfs /gentoo-20250101.xz.sqfs
-rm gentoo-20250101.xz.sqfs
-mksquashfs /squashfs-root /var/tmp/catalyst/snapshots/gentoo-20250101.sqfs
-rm -Rf /squashfs-root
+#if test "$REMOTE" = "local"; then 
+#   echo "local"
+#   wget http://192.168.2.102/gentoo/gentoo-20250101.xz.sqfs
+#else
+#   echo "remote"
+#   wget http://distfiles.gentoo.org/snapshots/squashfs/gentoo-20250101.xz.sqfs
+#fi
+mkdir /var/tmp/catalyst/snapshots
+catalyst --snapshot stable
+cp /var/tmp/catalyst/snapshots/gentoo-*.sqfs /var/tmp/catalyst/snapshots/gentoo-20250101.sqfs
+#mkdir -p /var/tmp/catalyst/snapshots
+#unsquashfs /gentoo-20250101.xz.sqfs
+#rm gentoo-20250101.xz.sqfs
+#mksquashfs /squashfs-root /var/tmp/catalyst/snapshots/gentoo-20250101.sqfs
+#rm -Rf /squashfs-root
 
 if test "$REMOTE" = "local"; then 
    echo "local"
@@ -86,8 +99,8 @@ sed -e 's|@TIMESTAMP@|20250101|g' \
     releng/releases/specs/amd64/installcd-stage2-minimal.spec
 #    releng/releases/specs/amd64/stage1-systemd-23.spec \
 #    releng/releases/specs/amd64/stage3-systemd-23.spec \
-#raise the job count to equal the core count
-# sed -i 's/# jobs = 4/jobs = '$(nproc)'/g' /etc/catalyst/catalyst.conf
+#raise the job count to equal the max core count for the size of the memory
+sed -i 's/# jobs = 4/jobs = '$MAXCORE'/g' /etc/catalyst/catalyst.conf
 # remove net-proxy/tsocks and dante from installcd specs do to GCC 14 compile issues
 sed -i '/socks5/d' /releng/releases/specs/amd64/installcd-stage1.spec
 sed -i '/tsocks/d' /releng/releases/specs/amd64/installcd-stage1.spec
