e26eef7365b9258244889d6c61ce675b2994d53c
ajherchenroder <136374399+ajherchenroder@users.noreply.github.com>
2025-10-28 11:26:39 -0400

    automate disk prep for the ALFS scritp

--- target/alfs.sh
+++ target/alfs.sh
@@ -10,19 +10,42 @@ chmod 777 /etc
 chmod 777 /sys
 chmod 777 /tmp
 chmod 777 /usr
-#lsblk
-#read -p "Enter the partition to build on (sdxx) -> " USEPART
-#if ! test -d /mnt
-#then   
-#   mkdir /mnt
-#fi
-#if ! test -d /mnt/buildroot
-#then
-#   mkdir /mnt/buildroot
-#fi
-#mount -v -t ext4 /dev/$USEPART /mnt/buildroot
-#cd /mnt/buildroot
+lsblk
+read -p 'Select the disk /dev node to put the bootstrap on.> ' DISKTOUSE
+sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk /dev/$DISKTOUSE
+  g # clear the in memory partition table
+  n # new partition
+  1 # partition number 1
+    # default - start at beginning of disk 
+  +1G # 1G uefi parttion
+  t # change type
+  1 # uefi
+  n # new partition
+  2 # partion number 2
+    # default, start immediately after preceding partition
+  +16G # swap partion
+  t # change type
+  19 # swap
+  n # new partition
+  4 # partion number 3
+    # default, start immediately after preceding partition
+    # default use the rest of the disk
+  w # write the partition table
+  q # and we're done
+EOF
+# Setup and enable a swap partition
+mkswap /dev/$DISKTOUSE'2'
+swapon /dev/$DISKTOUSE'2' 
+# format and mount the target partition
+mkfs.ext4 /dev/$DISKTOUSE''
+mkdir /mnt/lfs
+mount /dev/$DISKTOUSE'2' /mnt/lfs
+cd /mnt/lfs
+# format and mount the rest of the partitions
+mkfs.vfat /dev/$DISKTOUSE'1'
+mkdir /mnt/lfs/uefi
+mount /dev/$DISKTOUSE'1' /mnt/lfs/uefi
+# ready to start alfs
 git clone https://git.linuxfromscratch.org/jhalfs.git jhalfs
 cd jhalfs
 make
-echo "Ready to run ALFS"
